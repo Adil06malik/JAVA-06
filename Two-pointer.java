1de4526352a6de483af7f71a4c7f908b57e8ae97
Adil06malik <118048495+Adil06malik@users.noreply.github.com>
2022-11-30 22:18:36 -0800

    Create Two-pointer.java

---
+++ Two-pointer.java
@@ -0,0 +1,18 @@
+public class Twopointer {
+  
+    public static void main(String[] args) {
+        String s = "NO26 Sound";
+      
+        String r ="";
+        for(int i=0; i<=s.length()-1;i++){
+            char c = s.charAt(i);
+            if(!Character.isWhitespace(c));
+              r = r + c;
+        }
+        System.out.println(r);
+        
+    }
+    
+}
+
+
