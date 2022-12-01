10b16a080b27ffb398e33beb87cabad7f3dfdf52
Adil06malik <118048495+Adil06malik@users.noreply.github.com>
2022-11-30 22:53:19 -0800

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,19 @@
+public class lcm {
+   
+    public static void main(String[] args) {
+        int n1 = 48;
+        int n2 =18;
+      int count;
+
+        while(n1%n2!=0){
+         int rem= num%dev;
+         n2 = n1;
+         n1 = rem;
+
+        }
+        
+        System.out.println(n1);
+         
+    }
+    
+}
